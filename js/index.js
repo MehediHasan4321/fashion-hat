158f35932ea8435e5f9609dcc303d47b4e8cd799
MehediHasan4321 <126mehedi@gmail.com>
2023-03-09 14:03:59 +0600

    search function do workable and it working now

--- js/index.js
+++ js/index.js
@@ -1,28 +1,39 @@
-const sliderContainer = document.getElementById('custom-slider')
+//Global Variable...
+let count = 0
 let isOpen = true
-document.getElementById('shoping-icon').addEventListener('click',()=>{
-    if(isOpen){
-        sliderContainer.style.width = '350px'
-        isOpen = false
-    }else{
-        sliderContainer.style.width = '0px'
-        isOpen = true
-    }
-    
-})
-
-const loadProductData = ()=>{
-    fetch('https://fakestoreapi.com/products')
-    .then(respose=>respose.json())
-    .then(data=>displayData(data))
+let arr = []
+window.onload = () => {
+    loadProductData()
 }
-loadProductData()
-function displayData(products){
-    
+
+const loadProductData = () => {
+    fetch('https://fakestoreapi.com/products')
+        .then(respose => respose.json())
+        .then(data => {
+            displayData(data)
+            arr.push(data)
+        })
+}
+
+
+function productPrice(id) {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => {
+        calculatePrice(data)
+        count++
+        setInnerTextByIdAndValue('card-number', count)
+    })
+}
+console.log(arr)
+
+function displayData(products) {
+
     const productContainer = document.getElementById('product-cart-container')
-    for(const product of products){
-       // console.log(product)
-        const {image,title,price,id} = product
+    productContainer.innerHTML = ''
+    for (const product of products) {
+
+        const { image, title, price, id } = product
         productContainer.innerHTML += `
         <div class="w-[320px] h-[400px] p-1 bg-sky-200 rounded-md relative">
             <img class="w-full h-[200px]" src="${image}" alt="">
@@ -35,37 +46,55 @@ function displayData(products){
         `
     }
 }
-let count = 0
-function productPrice(id){
-    fetch(`https://fakestoreapi.com/products/${id}`)
-    .then(res=>res.json())
-    .then(data=>{
-        calculatePrice(data)
-        count ++
-        setInnerTextByIdAndValue('card-number',count)
-    })
-}
 
-function getInnerTextById(id){
+
+function getInnerTextById(id) {
     const elementString = document.getElementById(id).innerText;
     const stringToNumber = parseInt(elementString);
     return stringToNumber
 }
 
-function setInnerTextByIdAndValue(id,value){
+function setInnerTextByIdAndValue(id, value) {
     const element = document.getElementById(id);
     element.innerText = value
 }
 
-function calculatePrice(product){
-    const {price} = product
+function calculatePrice(product) {
+    const { price } = product
     const oldPrice = getInnerTextById('price');
     const newPrice = price;
     const totalPrice = oldPrice + newPrice
     const shopingCost = getInnerTextById('shiping-cost')
-    const totalVat = (totalPrice + shopingCost)*15/100;
+    const totalVat = (totalPrice + shopingCost) * 15 / 100;
     const totalPayableAmount = totalPrice + shopingCost + totalVat
-    setInnerTextByIdAndValue('price',totalPrice.toFixed(2))
-    setInnerTextByIdAndValue('total-vat',totalVat.toFixed(2))
-    setInnerTextByIdAndValue('total-payable-price',totalPayableAmount.toFixed(2))
+    setInnerTextByIdAndValue('price', totalPrice.toFixed(2))
+    setInnerTextByIdAndValue('total-vat', totalVat.toFixed(2))
+    setInnerTextByIdAndValue('total-payable-price', totalPayableAmount.toFixed(2))
 }
+
+
+
+document.getElementById('shoping-icon').addEventListener('click', () => {
+    const sliderContainer = document.getElementById('custom-slider')
+    if (isOpen) {
+        sliderContainer.style.width = '350px'
+        isOpen = false
+    } else {
+        sliderContainer.style.width = '0px'
+        isOpen = true
+    }
+
+})
+
+document.getElementById("search-inp").addEventListener("keyup",function(e){
+    const value = this.value.toLowerCase();
+    const newArray = arr[0].filter(product=>{
+        return product.title.toLowerCase().includes(value)
+    })
+    
+    if(e.keyCode ===13){
+        displayData(newArray)
+    }
+    
+})
+
